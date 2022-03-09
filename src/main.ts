e2314d52e25244f7147665a192b27b3303dce078
Ganessh Kumar R P <garp@microsoft.com>
2022-03-09 16:01:27 +0530

    support for opening the editor below or right of active leaf

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Menu, Plugin } from 'obsidian';
 import { TableView, MARKDOWN_TABLE_EDITOR_VIEW } from "./view";
+
+const VERTICAL_EDITOR_TEXT = 'Open Editor (Next to the Active View)';
+const HORIZONTAL_EDITOR_TEXT = 'Open Editor (Below the Active View)';
 
 export default class MarkdownTableEditorPlugin extends Plugin {
 	async onload() {
@@ -8,15 +11,49 @@ export default class MarkdownTableEditorPlugin extends Plugin {
       (leaf) => new TableView(leaf)
     );
 
-    this.addRibbonIcon("pane-layout", "Open Markdown Table Editor", () => {
-      this.activateView();
+    this.addRibbonIcon("pane-layout", "Open Markdown Table Editor", (event) => {
+      if (event.type == 'click') {
+        this.activateView('vertical');
+        event.preventDefault();
+        return;
+      }
+
+      const menu = new Menu(this.app);
+
+      menu.addItem((item) =>
+        item
+          .setTitle(VERTICAL_EDITOR_TEXT)
+          .setIcon('vertical-split')
+          .onClick(() => {
+            this.activateView('vertical');
+          })
+      );
+
+      menu.addItem((item) =>
+        item
+        .setTitle(HORIZONTAL_EDITOR_TEXT)
+        .setIcon('horizontal-split')
+        .onClick(() => {
+          this.activateView('horizontal');
+        })
+      );
+
+      menu.showAtMouseEvent(event);
     });
 
     this.addCommand({
-      id: 'markdown-table-editor-open',
-			name: 'Open Editor',
-			editorCallback: async (editor: Editor, view: MarkdownView) => {
-				this.activateView();
+      id: 'markdown-table-editor-open-vertical',
+			name: VERTICAL_EDITOR_TEXT,
+			editorCallback: async (_: Editor, __: MarkdownView) => {
+				this.activateView('vertical');
+			}
+    });
+
+    this.addCommand({
+      id: 'markdown-table-editor-open-horizontal',
+			name: HORIZONTAL_EDITOR_TEXT,
+			editorCallback: async (_: Editor, __: MarkdownView) => {
+				this.activateView('horizontal');
 			}
     });
 	}
@@ -25,7 +62,7 @@ export default class MarkdownTableEditorPlugin extends Plugin {
 		this.app.workspace.detachLeavesOfType(MARKDOWN_TABLE_EDITOR_VIEW);
 	}
 
-	async activateView() {
+	async activateView(direction: 'vertical' | 'horizontal' = 'vertical') {
     this.app.workspace.detachLeavesOfType(MARKDOWN_TABLE_EDITOR_VIEW);
 
 		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -33,8 +70,10 @@ export default class MarkdownTableEditorPlugin extends Plugin {
 			return;
 		}
 
-		var data = view.editor.getSelection();
-    await this.app.workspace.getLeaf(true).setViewState({
+		const data = view.editor.getSelection();
+    const activeLeaf = this.app.workspace.activeLeaf;
+    
+    await this.app.workspace.createLeafBySplit(activeLeaf, direction).setViewState({
       type: MARKDOWN_TABLE_EDITOR_VIEW,
       active: true,
 			state: { data }
